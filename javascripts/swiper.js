5c2803871e113edc7945b9c87492fec6a8485d45
Eden-sth <cseo99737@gmail.com>
2025-08-19 15:22:50 +0900

    모나미 클론 재작업

---
+++ javascripts/swiper.js
@@ -0,0 +1,27 @@
+const smBanner = new Swiper(".sw-banner", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  //  centeredSlides: true,
+  //  loop: true,
+  //  speed: 1000,
+  //  autoplay: {
+  //   delay: 2500,
+  //   // speed 와 혼돈 주의
+  //   // speed란 속성은 속도 조절 기능
+  //   disableOnInteraction: false,
+  //   // false로 해야 슬라이드 자동으로 넘어감
+  //  },
+});
+
+// const swBannerElement = document.querySelector(".sw-banner");
+// const swPlayBtn = document.querySelector(".playBtn");
+// const swPauseBtn = document.querySelector(".pauseBtn");
+
+// 마우스 올리면 일시정지 기능
+// swBannerElement.addEventListener("mouseover", () => {
+// swBanner.autoplay.stop();
+// });
+// 마우스 벗어나면 재생 기능
+// swBannerElement.addEventListener("mouseout", () => {
+// swBanner.autoplay.start();
+// });
